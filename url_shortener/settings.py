adcdeeee990f211008c3ec3931b5b1af978db412
dorianfong98 <47025155+dorianfong98@users.noreply.github.com>
2022-08-31 22:23:31 +0800

    Update settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
